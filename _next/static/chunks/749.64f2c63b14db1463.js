da209c924e2116231dce4a51a4cb5477f76a7a77
VirtualOfficeOrg <>
2023-08-03 04:51:50 +0000

    Update from https://github.com/VirtualOfficeOrg/frontend/commit/32a6361a4b58c14a14865b403c188721711c435d

---
+++ _next/static/chunks/749.64f2c63b14db1463.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[749],{9749:function(e,n,t){t.r(n),t.d(n,{default:function(){return l}});var u=t(5893),i=t(7294),r=t(8417),c=t(3271);function l(e){let{children:n}=e,t=i.useMemo(()=>(0,r.Z)({key:"with-tailwind",insertionPoint:document.querySelector("head").firstChild}),[]);return(0,u.jsx)(c.C,{value:t,children:n})}}}]);
